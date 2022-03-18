351bc40e77259311c99eafece585a33b5a63089c
gabrielbarita <gabrielbarita@gmail.com>
2022-03-18 09:11:27 +0700

    gabrielbarita: added questionOfTheDay template

--- homepage.js
+++ homepage.js
@@ -29,7 +29,6 @@ dotThree.addEventListener('click', () => {
 function showSlides(n) {
 	let i;
 	let slides = document.querySelectorAll('.main-slideshow-media');
-	console.log(slides);
 	let dots = document.querySelectorAll('.dot');
 	if (n > slides.length) {
 		slideIndex = 1;
@@ -144,3 +143,81 @@ subjectRight.addEventListener('click', function () {
 subjectLeft.addEventListener('click', function () {
 	document.querySelector('.subject-buttons').scrollLeft -= 615;
 });
+
+// posts of the day
+
+const postInfo = {
+	postTitles: [
+		'What is on your mind?',
+		'Why do echo chambers exist?',
+		`Has the Big Bang theory been 100% scientifically proven yet?`,
+	],
+	postDescs: [
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia velit sapien, eget venenatis justo vulputate sit amet. Aenean enim leo, consequat quis vestibulum in, eleifend fermentum magna.',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia velit sapien, eget venenatis justo vulputate sit amet. Aenean enim leo, consequat quis vestibulum in, eleifend fermentum magna.',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin lacinia velit sapien, eget venenatis justo vulputate sit amet. Aenean enim leo, consequat quis ',
+	],
+	topReplies: [
+		'insert the answer to what is on your mind',
+		'insert the answer to why echo chambers exist',
+		'insert the answer to the reason the big bang theory is accurate',
+	],
+	posterNames: ['LEARNERNAME', 'GENERICCARP', 'WHOSHALLNOTBENAMED'],
+	likeCounts: ['1.3K', '598', '789'],
+	commentCounts: ['2.2K', '999', '405'],
+	categoryNames: ['GENERAL KNOWLEDGE', 'POLITICS', 'PHYSICS'],
+};
+
+document.querySelector('.top-questions').innerHTML = '';
+
+for (let i = 0; i < postInfo.postTitles.length; i++) {
+	const threadBox = document.createElement('div');
+
+	const leftSide = document.createElement('div');
+	const rightSide = document.createElement('div');
+	const postTitle = document.createElement('h3');
+	const postDesc = document.createElement('p');
+	const topReply = document.createElement('p');
+	const postButton = document.createElement('button');
+	const posterName = document.createElement('h3');
+	const likeCount = document.createElement('p');
+	const commentCount = document.createElement('p');
+	const categoryName = document.createElement('h4');
+
+	threadBox.appendChild(leftSide);
+	threadBox.appendChild(rightSide);
+	leftSide.appendChild(postTitle);
+	leftSide.appendChild(postDesc);
+	leftSide.appendChild(topReply);
+	leftSide.appendChild(postButton);
+	rightSide.appendChild(posterName);
+	rightSide.appendChild(likeCount);
+	rightSide.appendChild(commentCount);
+	rightSide.appendChild(categoryName);
+
+	threadBox.classList.add('thread-box');
+	leftSide.classList.add('top-question-left-side');
+	rightSide.classList.add('top-question-right-side');
+	postButton.classList.add('top-question-button');
+
+	postTitle.innerHTML = `${postInfo.postTitles[i]}`;
+	postDesc.innerHTML = `${postInfo.postDescs[i]}`;
+	topReply.innerHTML = `<b>Top reply:</b> ${postInfo.topReplies[i]}`;
+	postButton.innerHTML = `More replies`;
+	posterName.innerHTML = `BY ${postInfo.posterNames[i]}`;
+	likeCount.innerHTML = `${postInfo.likeCounts[i]} LIKES`;
+	commentCount.innerHTML = `${postInfo.commentCounts[i]} COMMENTS`;
+	categoryName.innerHTML = `${postInfo.categoryNames[i]}`;
+
+	postButton.addEventListener('mouseover', () => {
+		postButton.style.transform = 'scale(1.05)';
+		postButton.style.backgroundColor = 'black';
+		postButton.addEventListener('mouseout', () => {
+			postButton.style.transform = 'scale(1.0)';
+			postButton.style.backgroundColor = 'rgb(128, 224, 248)';
+		});
+		console.log('works');
+	});
+
+	document.querySelector('.top-questions').appendChild(threadBox);
+}
